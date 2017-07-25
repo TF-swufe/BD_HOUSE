c1ac0c459e2ed8c61c1f4af4e8d1184911907c77
yuandafu <443269860@qq.com>
2017-07-25 15:37:10 +0800

    测试提交

---
+++ BD_HOUSE/src/cn/tfswufe/test/PrintTest.java
@@ -0,0 +1,13 @@
+package cn.tfswufe.test;
+
+public class PrintTest {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("2017Äê7ÔÂ25ÈÕ15:34:28");
+	}
+
+}
